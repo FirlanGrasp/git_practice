28b7ef21fd23aa503cdb317847417fad25212263
FirlanGrasp <chenshan_email.foxmail.com>
2025-03-29 12:33:26 +0800

    初始化

---
+++ src/test/java/com/git/git_practice/GitPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.git_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
